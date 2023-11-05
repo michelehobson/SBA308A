fb3b0a80829c19f63f90ea11fe4f489e340d4a79
michelehobson <michelehobson@hotmail.com>
2023-11-05 15:49:32 -0500

    Revamped and APIs Working

--- src/main.js
+++ src/main.js
@@ -1,151 +1,137 @@
 import * as Location from './modLocation.js';
 // import * as Weather from './modWeather.js';
-import data from './database.json' assert {type: 'json'}
 
-const whereTo = document.querySelector('div:nth-of-type(1)');
-const regions = document.querySelector('div:nth-of-type(2)');
-const cities = document.querySelector('div:nth-of-type(3)');
+var key = countryConfig.KEY;
+var host = countryConfig.HOST;
 
-const country = document.querySelector('select');
+// const whereTo = document.querySelector('div:nth-of-type(1)');
+// const regions = document.querySelector('div:nth-of-type(2)');
+// const cities = document.querySelector('div:nth-of-type(3)');
+const wToSelect = document.querySelector('select:nth-of-type(1)');
+const regSelect = document.getElementById('selectRegion');
+const citySelect = document.getElementById('selectCity');
+
+const country = document.querySelector('select:nth-of-type(1)');
+const region = document.getElementById('selectRegion');
+const city = document.getElementById('selectCity');
+
 let countryIso = '';
-
 let regionIso = '';
-let regionLong;
-let regionLat;
 let cityIso = '';
-let cityLong;
-let cityLat;
 
-let regionArray = [[],[]]; //
-let cityArray = [[],[]];
+if(wToSelect.length === 0) {
+   Location.getCountries(wToSelect);
+}
 
-const optionBlank = document.createElement("option");
-country.appendChild(optionBlank);
+async function getStates(country) {
+   const states = await axios.get(`https://country-state-city-search-rest-api.p.rapidapi.com/states-by-countrycode`,
+       {
+           params: {countrycode: country},
+           headers: {
+               'X-RapidAPI-Key': key,
+               'X-RapidAPI-Host': host
+           },
+       }
+   )
+   if(states !== null) {
+       console.log('states !== null')
+       const regionOptionBlank = document.createElement("option");
+       regSelect.appendChild(regionOptionBlank);
+       states.data.forEach((region) => {
+          const regionOption = document.createElement("option");
+          regionOption.text = region.name;
+          regionOption.value = region.isoCode;
+          regSelect.appendChild(regionOption);
+       });
+   }
+}
 
-loadCountries();
-
-function loadCountries() {
-   try {
-      let countries = data;
-      let index = 0;
-      for(const c in countries) {
-         const option = document.createElement("option");
-         option.text = data[index].country;
-         option.value = data[index].iso2
-         country.appendChild(option);
-         index++;
-      }
-   } catch(error) {
-      console.log(error)
+async function getCities(country, regionCode) {
+   const cities = await axios.get(`https://country-state-city-search-rest-api.p.rapidapi.com/cities-by-countrycode-and-statecode`,
+       {
+           params: {
+               countrycode: country,
+               statecode: regionCode
+             },            
+           headers: {
+               'X-RapidAPI-Key': key,
+               'X-RapidAPI-Host': host
+           },
+       }
+   )
+   if(cities !== null) {
+       const cityOptionBlank = document.createElement("option");
+       citySelect.appendChild(cityOptionBlank);
+       cities.data.forEach((city) => {
+          const cityOption = document.createElement("option");
+          cityOption.text = city.name;
+          cityOption.value = city.latitude.concat(',').concat(city.longitude);
+          citySelect.appendChild(cityOption);
+       });
    }
 }
 
 country.addEventListener('change', async (e) => {
+   console.log('County Triggered')
    e.preventDefault();
-   if(countryIso !== country.value) { //Prevent unnecessay API calls
+   if(country.value !== countryIso) {
       if(country.value.length > 0) {
          countryIso = country.value;
-         console.log(country.value)
-
-         let loadRegions = Location.getStates(countryIso);
-         console.log(loadRegions)
-         if(loadRegions !== null) {
-            const select = document.createElement('select');
-            regions.innerHTML = 'Region';
-            regions.classList.add('regions');
-            regions.appendChild(select);
-            const regionalData = await loadRegions.json();
-            regionalData.array.forEach(element => {
-               const option = document.createElement("option");
-               option.text = element.name;
-               option.value = element.isoCode;
-               regions.appendChild(option);
-               regionArray.push(element.isoCode, element.latitude, element.longitude)
-            });
-         }
+         console.log(countryIso)
+         clearRegions();
+         getStates(countryIso);
       } else {
          countryIso = '';
-         cityIso = '';
-         cities.removeChild(cities.firstChild); //Removes the label City
-         cities.removeChild(cities.firstChild); //Removes the dropdown
-         regionIso = '';
-         regions.removeChild(regions.firstChild); //Removes the label Region
-         regions.removeChild(regions.firstChild); //Removes the dropdown
-         regionArray = [[],[]];
-         cityArray = [[],[]];
-
-      }
-   }
-})
-
-regions.addEventListener('change', async (e) => {
-   e.preventDefault();
-   if(regionArray.length === 0) { //Prevent unnecessay API calls
-      cityArray = [[],[]];
-      if(regions.value.length > 0) {
-         regionIso = regions.value;
-         regionLat = re
-         console.log(regions.value)
-         let loadCities = Location.getCities(countryIso, regionIso);
-         console.log(loadCities)
-         if(loadRegions !== null) {
-            const select = document.createElement('select');
-            cities.innerHTML = 'City';
-            cities.classList.add('cities');
-            cities.appendChild(select);
-            const cityData = await loadCities.json();
-            cityData.array.forEach(element => {
-               const option = document.createElement("option");
-               option.text = element.name;
-               option.value = element.isoCode;
-               cities.appendChild(option);
-               cityArray.push(element.isoCode, element.latitude, element.longitude)
-            });
-         }
-      } else {
-         regionIso = '';
-         cities.removeChild(cities.firstChild); //Removes the label City
-         cities.removeChild(cities.firstChild); //Removes the dropdown
-         regionArray = [[],[]];
-         cityArray = [[],[]];
+         clearRegions();
       }
    } else {
-      let notFound = true;
-      let i = 0;
-      while (notFound) {
-         if (regionArray[i][0] === regions.value)
-            
-      }
+      countryIso = '';
+      clearRegions();
    }
 })
 
-cities.addEventListener('change', async (e) => { //Prevent unnecessay API calls
-   e.preventDefault()
-   if(cityArray.length === 0) {
-      if(cities.value.length > 0) {
-         cityIso = cities.value;
-         console.log(cities.value)
-         let loadCities = Location.getCities(countryIso, regionIso);
-         console.log(loadCities)
-         if(loadRegions !== null) {
-            const select = document.createElement('select');
-            cities.innerHTML = 'City';
-            cities.classList.add('cities');
-            cities.appendChild(select);
-            const cityData = await loadCities.json();
-            cityData.array.forEach(element => {
-               const option = document.createElement("option");
-               option.text = element.name;
-               option.value = element.isoCode;
-               cities.appendChild(option);
-            });
-         }
+region.addEventListener('change', async (e) => {
+   console.log("Region Triggered")
+   e.preventDefault();
+   if(region.value !== regionIso) {
+      if(region.value.length > 0) {
+         regionIso = region.value;
+         clearCities();
+         getCities(countryIso, regionIso)
       } else {
-         regionIso = '';
-         cities.removeChild(cities.firstChild); //Removes the label City
-         cities.removeChild(cities.firstChild); //Removes the dropdown
-         cityArray = [[],[]];
-
+         clearRegions();
       }
+   } else {
+      clearRegions();
    }
 })
+
+city.addEventListener('change', async (e) => { //Prevent unnecessay API calls
+   console.log("City Triggered")
+
+   e.preventDefault()
+   if(city.value !== cityIso) {
+      if(city.value.length > 0) {
+         cityIso = city.value;
+      } else {
+         clearCities();
+      }
+   } else {
+      clearCities();
+   }
+})
+
+let clearCities = () => {
+   console.log('Clearing Cities')
+   cityIso = '';
+   city.length = 0;
+
+}
+let clearRegions = () => {
+   console.log('Clearing Regions & Cities')
+   regionIso = '';
+   cityIso = '';
+   region.length = 0;
+   city.length = 0;
+
+}
--- src/modLocation.js
+++ src/modLocation.js
@@ -1,70 +1,26 @@
 var key = countryConfig.KEY;
 var host = countryConfig.HOST;
-const selection = document.querySelector('select');
 
-// Country Details
-// capital, code, callingCode, currencyCode, flagImageUrl, name, numRegions, wikiDataId
-export async function getDemographics(country) {
-    const demographics = {
-        method: 'GET',
-        url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${country}`,
-        headers: {
-            'X-RapidAPI-Key': key,
-            'X-RapidAPI-Host': host
-        }
-    };
-
-    try {
-        const response = await axios.request(demographics);
-        console.log(response.data);
-    } catch(error) {
-        console.error(error);
-    }
-}
-
-// Country Regions
-// Returns States
-// name, isoCode, countryCode, latitude, longitude
-export async function getStates(country) {
-    const states = await axios.get(`https://country-state-city-search-rest-api.p.rapidapi.com/states-by-countrycode`,
+export async function getCountries(wToSelect) {
+    const countries = await axios.get(`https://country-state-city-search-rest-api.p.rapidapi.com/allcountries`,
         {
-            params: {countrycode: country},
             headers: {
                 'X-RapidAPI-Key': key,
                 'X-RapidAPI-Host': host
             },
         }
     )
-    return states.data;
-
-    try {
-        const response = await axios.request(states);
-        console.log(response.data);
-    } catch(error) {
-        console.error(error);
+    if(countries !== null) {
+        console.log('countries !== null')
+        const countryOptionBlank = document.createElement("option");
+        wToSelect.appendChild(countryOptionBlank);
+        countries.data.forEach((region) => {
+           const countryOption = document.createElement("option");
+           countryOption.text = region.name;
+           countryOption.value = region.isoCode;
+           wToSelect.appendChild(countryOption);
+        });
     }
 }
 
-// Country Region Cities
-// id, wikiDataId, type, city, name, longitude, latitude, population
-// namePrefix pulled in specific city
-export async function getCities(country, regionCode, city) {
-    const cities = {
-        method: 'GET',
-        url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${country}/regions/${regionCode}/cities`,
-        params: {
-            namePrefix: city
-        },
-        headers: {
-            'X-RapidAPI-Key': key,
-            'X-RapidAPI-Host': host
-        }
-    };
 
-    try {
-        const response = await axios.request(cities);
-        console.log(response.data);
-    } catch(error) {
-        console.error(error);
-    }
-}
