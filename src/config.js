4ca5c2cfbd8e56b64e860d70b5367c85909942ac
michelehobson <michelehobson@hotmail.com>
2023-11-16 09:32:59 -0500

    Gitignore

---
+++ src/config.js
@@ -0,0 +1,13 @@
+var weatherConfig = {
+    KEY: '0f68076deemshe36c509ac9073d9p16be1cjsn6f4aa7c9edc6',
+    HOST: 'weatherapi-com.p.rapidapi.com'
+}
+
+countryConfig = {
+    KEY: '0f68076deemshe36c509ac9073d9p16be1cjsn6f4aa7c9edc6',
+    HOST: 'country-state-city-search-rest-api.p.rapidapi.com'
+}
+
+pexelConfig = {
+    KEY: 'myAQPEXbDdQcFE3iFivVdefoU2OF9KlAJM6LbU2OJYI5qWv39uy8jBSq'
+}
